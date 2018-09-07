9b556a2b292d2cf40cb1af66d4b620e2da7ec1ac
Callan Moore <CallanGameDev@gmail.com>
2018-09-07 13:12:22 +1200

    Setup Visor in C++ Start creating input actions for C++

--- Source/VRProto/PawnVR.cpp
+++ Source/VRProto/PawnVR.cpp
@@ -1,27 +1,55 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PawnVR.h"
+#include "Engine/Classes/Engine/TextureRenderTarget2D.h"
 
 // Sets default values
 APawnVR::APawnVR()
 	: Super()
 {
+	// Find the relevant meshes
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>PlaneMesh(TEXT("StaticMesh'/Engine/BasicShapes/Plane.Plane'"));
+
+	// VR Origin creation and basic setup
 	VR_Origin = CreateDefaultSubobject<USceneComponent>(TEXT("VR_Origin"));
 	VR_Origin->SetupAttachment(GetRootComponent());
 	VR_Origin->RelativeLocation = FVector(0.0f, 0.0f, 64.0f);
 	
+	// The Main VR Camera creation and basic setup
 	Camera_VR = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera_VR"));
 	Camera_VR->SetupAttachment(VR_Origin);
 
-	Camera_Sniper = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera_Sniper"));
-	Camera_Sniper->SetupAttachment(Camera_VR);
+	// Creating and placing the Visor - Zoom Panel
+	VisorMesh_ZoomPanel = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VisorMesh_ZoomPanel"));
+	VisorMesh_ZoomPanel->SetupAttachment(Camera_VR);
+	VisorMesh_ZoomPanel->SetStaticMesh(PlaneMesh.Object);	
+	VisorMesh_ZoomPanel->SetRelativeLocation(FVector(200.0f, 0.0f, 0.0f));
+	VisorMesh_ZoomPanel->SetRelativeRotation(FRotator(0.0f, 90.0f, 90.0f));
+	VisorMesh_ZoomPanel->SetRelativeScale3D(FVector(1.7777f, 1.0f, 1.0f));
+	VisorMesh_ZoomPanel->SetVisibility(false, true);
+	
+	// Creating and placing the Visor - Obscuring Panel to create relative space that is unchanging when zoomed in
+	VisorMesh_ObscuringPanel = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VisorMesh_ObscuringPanel"));
+	VisorMesh_ObscuringPanel->SetupAttachment(VisorMesh_ZoomPanel);
+	VisorMesh_ObscuringPanel->SetStaticMesh(PlaneMesh.Object);
+	VisorMesh_ObscuringPanel->SetRelativeLocation(FVector(0.0f, 0.0f, -5.0f));
+	VisorMesh_ObscuringPanel->SetRelativeRotation(FRotator(0.0f, 0.0f, 0.0f));
+	VisorMesh_ObscuringPanel->SetRelativeScale3D(FVector(3.0f, 3.0f, 3.0f));
+	VisorMesh_ObscuringPanel->SetVisibility(false, true);
+
+	// Creating and placing the Capture component for the zoom capture
+	VisorCapture = CreateDefaultSubobject<USceneCaptureComponent2D>(TEXT("Visor_Capture"));
+	VisorCapture->SetupAttachment(Camera_VR);
+	VisorCapture->SetRelativeLocation(FVector(1.0f, 0.0f, 0.0f));
+	VisorCapture->HideComponent(VisorMesh_ZoomPanel);
+	VisorCapture->HideComponent(VisorMesh_ObscuringPanel);
+
 }
 
 // Called when the game starts or when spawned
 void APawnVR::BeginPlay()
 {
 	Super::BeginPlay();
-
 }
 
 // Called every frame
@@ -57,6 +85,36 @@ void APawnVR::Movement_Right(float value)
 		// Add movement in the direction
 		AddMovementInput(GetActorRightVector(), value);
 	}
+}
+
+void APawnVR::View_X(float value)
+{
+
+}
+
+void APawnVR::View_Y(float value)
+{
+
+}
+
+void APawnVR::LeftTrigger_Pressed(float value)
+{
+
+}
+
+void APawnVR::LeftTrigger_Released(float value)
+{
+
+}
+
+void APawnVR::RightTrigger_Pressed(float value)
+{
+
+}
+
+void APawnVR::RightTrigger_Released(float value)
+{
+
 }
 
 
--- Source/VRProto/PawnVR.h
+++ Source/VRProto/PawnVR.h
@@ -5,6 +5,8 @@
 #include "CoreMinimal.h"
 #include "GamePawn.h"
 #include "Engine/Classes/Camera/CameraComponent.h"
+#include "Engine/Classes/Components/SceneCaptureComponent2D.h"
+#include "Runtime/CoreUObject/Public/UObject/ConstructorHelpers.h"
 #include "PawnVR.generated.h"
 
 
@@ -23,18 +25,26 @@ public: // Functionality
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 public: // Variables
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Components)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "ViewPort")
 		USceneComponent* VR_Origin;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "ViewPort")
+		UCameraComponent* Camera_VR;	
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Components)
-		UCameraComponent* Camera_VR;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Components)
-		UCameraComponent* Camera_Sniper;
-
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "VisorZoom")
+		UStaticMeshComponent* VisorMesh_ZoomPanel;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "VisorZoom")
+		UStaticMeshComponent* VisorMesh_ObscuringPanel;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "VisorZoom")
+		USceneCaptureComponent2D* VisorCapture;
 
 protected:
 	virtual void BeginPlay() override;
 	virtual void Movement_Forward(float value);
 	virtual void Movement_Right(float value);
+	virtual void View_X(float value);
+	virtual void View_Y(float value);
+	virtual void LeftTrigger_Pressed(float value);
+	virtual void LeftTrigger_Released(float value);
+	virtual void RightTrigger_Pressed(float value);
+	virtual void RightTrigger_Released(float value);
 };
